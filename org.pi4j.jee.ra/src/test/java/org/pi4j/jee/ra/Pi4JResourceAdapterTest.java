210fc5812d10fb48db0d882b320c33984d373027
Greg Lanthier <greg.lanthier@gmail.com>
2015-05-23 12:45:28 -0400

    a potentially cleaner way of mocking

--- org.pi4j.jee.ra/src/test/java/org/pi4j/jee/ra/Pi4JResourceAdapterTest.java
+++ org.pi4j.jee.ra/src/test/java/org/pi4j/jee/ra/Pi4JResourceAdapterTest.java
@@ -1,5 +1,25 @@
 package org.pi4j.jee.ra;
 
+/*
+ * #%L
+ * org.pi4j.jee.ra
+ * %%
+ * Copyright (C) 2015 Greg Lanthier
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
@@ -14,15 +34,11 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioProvider;
-import com.pi4j.io.gpio.Pin;
-import com.pi4j.io.gpio.PinMode;
-import com.pi4j.io.gpio.PinPullResistance;
-import com.pi4j.io.gpio.PinState;
-import com.pi4j.io.gpio.event.PinListener;
 
 @RunWith( MockitoJUnitRunner.class )
 public class Pi4JResourceAdapterTest {
@@ -39,134 +55,8 @@ public class Pi4JResourceAdapterTest {
 
 	@BeforeClass
 	public static void beforeClass() {
-		GpioFactory.setDefaultProvider( new GpioProvider() {
-			
-			@Override
-			public void unexport(Pin pin) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void shutdown() {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void setValue(Pin pin, double value) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void setState(Pin pin, PinState state) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void setPwm(Pin pin, int value) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void setPullResistance(Pin pin, PinPullResistance resistance) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void setMode(Pin pin, PinMode mode) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void removeListener(Pin pin, PinListener listener) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void removeAllListeners() {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public boolean isShutdown() {
-				// TODO Auto-generated method stub
-				return false;
-			}
-			
-			@Override
-			public boolean isExported(Pin pin) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-			
-			@Override
-			public boolean hasPin(Pin pin) {
-				// TODO Auto-generated method stub
-				return false;
-			}
-			
-			@Override
-			public double getValue(Pin pin) {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-			
-			@Override
-			public PinState getState(Pin pin) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public int getPwm(Pin pin) {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-			
-			@Override
-			public PinPullResistance getPullResistance(Pin pin) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public String getName() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public PinMode getMode(Pin pin) {
-				// TODO Auto-generated method stub
-				return null;
-			}
-			
-			@Override
-			public void export(Pin pin, PinMode mode, PinState defaultState) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void export(Pin pin, PinMode mode) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void addListener(Pin pin, PinListener listener) {
-				// TODO Auto-generated method stub
-				
-			}
-		});
+		m_mockGpioProvider = Mockito.mock( GpioProvider.class );
+		GpioFactory.setDefaultProvider( m_mockGpioProvider );
 	}
 	
 	@Test
