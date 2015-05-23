d2c133c773263693d8f19f966fd443320637bd53
Greg Lanthier <greg.lanthier@gmail.com>
2015-05-23 11:02:01 -0400

    edit

--- org.pi4j.jee.ra/src/main/java/org/pi4j/jee/ra/Pi4JResourceAdapter.java
+++ org.pi4j.jee.ra/src/main/java/org/pi4j/jee/ra/Pi4JResourceAdapter.java
@@ -1,5 +1,49 @@
 package org.pi4j.jee.ra;
 
-public class Pi4JResourceAdapter {
+import javax.resource.ResourceException;
+import javax.resource.spi.ActivationSpec;
+import javax.resource.spi.BootstrapContext;
+import javax.resource.spi.Connector;
+import javax.resource.spi.ResourceAdapter;
+import javax.resource.spi.ResourceAdapterInternalException;
+import javax.resource.spi.endpoint.MessageEndpointFactory;
+import javax.transaction.xa.XAResource;
+
+@Connector
+public class Pi4JResourceAdapter implements ResourceAdapter {
+
+	@Override
+	public void start(BootstrapContext ctx)
+			throws ResourceAdapterInternalException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endpointActivation(MessageEndpointFactory endpointFactory,
+			ActivationSpec spec) throws ResourceException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endpointDeactivation(MessageEndpointFactory endpointFactory,
+			ActivationSpec spec) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public XAResource[] getXAResources(ActivationSpec[] specs)
+			throws ResourceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
