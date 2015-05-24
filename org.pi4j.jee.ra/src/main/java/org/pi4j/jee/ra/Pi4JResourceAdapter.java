810b6789a29e3b9989cd992febb13d43ad89a149
Greg Lanthier <greg.lanthier@gmail.com>
2015-05-24 08:42:44 -0400

    checkstyle edits...

--- org.pi4j.jee.ra/src/main/java/org/pi4j/jee/ra/Pi4JResourceAdapter.java
+++ org.pi4j.jee.ra/src/main/java/org/pi4j/jee/ra/Pi4JResourceAdapter.java
@@ -9,9 +9,9 @@ package org.pi4j.jee.ra;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,31 +33,31 @@ import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 
 @Connector
-public class Pi4JResourceAdapter implements ResourceAdapter {
+public final class Pi4JResourceAdapter implements ResourceAdapter {
 
-  GpioController m_gpio = null;
+  private GpioController gpio = null;
 
   @Override
-  public void start( BootstrapContext ctx ) throws ResourceAdapterInternalException {
-    m_gpio = GpioFactory.getInstance( );
+  public void start( final BootstrapContext ctx ) throws ResourceAdapterInternalException {
+    gpio = GpioFactory.getInstance( );
   }
 
   @Override
   public void stop( ) {
-    m_gpio.shutdown( );
+    gpio.shutdown( );
   }
 
   @Override
-  public void endpointActivation( MessageEndpointFactory endpointFactory, ActivationSpec spec )
+  public void endpointActivation( final MessageEndpointFactory endpointFactory, final ActivationSpec spec )
       throws ResourceException {
   }
 
   @Override
-  public void endpointDeactivation( MessageEndpointFactory endpointFactory, ActivationSpec spec ) {
+  public void endpointDeactivation( final MessageEndpointFactory endpointFactory, final ActivationSpec spec ) {
   }
 
   @Override
-  public XAResource[ ] getXAResources( ActivationSpec[ ] specs ) throws ResourceException {
+  public XAResource[ ] getXAResources( final ActivationSpec[ ] specs ) throws ResourceException {
     return new XAResource[0];
   }
 
