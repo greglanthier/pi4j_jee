00412074964cba13ee854d4d9e80db61938002c4
Greg Lanthier <greg.lanthier@gmail.com>
2015-05-26 20:35:15 -0400

    Add new ManagedConnection implementation

---
+++ org.pi4j.jee.ra/src/main/java/org/pi4j/jee/ra/Pi4JManagedConnection.java
@@ -0,0 +1,111 @@
+package org.pi4j.jee.ra;
+
+/*
+ * #%L
+ * org.pi4j.jee.ra
+ * %%
+ * Copyright (C) 2015 Greg Lanthier
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.resource.ResourceException;
+import javax.resource.spi.ConnectionEventListener;
+import javax.resource.spi.ConnectionRequestInfo;
+import javax.resource.spi.LocalTransaction;
+import javax.resource.spi.ManagedConnection;
+import javax.resource.spi.ManagedConnectionMetaData;
+import javax.security.auth.Subject;
+import javax.transaction.xa.XAResource;
+
+public class Pi4JManagedConnection implements ManagedConnection {
+
+  List< ConnectionEventListener > listeners = new ArrayList< ConnectionEventListener >( );
+
+  /*
+   * Answer a connection handle. Multiple connection handles could be tied to a
+   * single managed connection.
+   * 
+   * For a CCI resource adapter this will be an object that implements {@link
+   * javax.resource.cci.Connection}.
+   */
+  @Override
+  public Object getConnection( final Subject _subject, final ConnectionRequestInfo _cxRequestInfo )
+      throws ResourceException {
+    return new Pi4JConnection( this );
+  }
+
+  @Override
+  public void destroy( ) throws ResourceException {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void cleanup( ) throws ResourceException {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void associateConnection( final Object _connection ) throws ResourceException {
+  }
+
+  @Override
+  public void addConnectionEventListener( final ConnectionEventListener _listener ) {
+    synchronized ( listeners ) {
+      listeners.add( _listener );
+    }
+  }
+
+  @Override
+  public void removeConnectionEventListener( final ConnectionEventListener _listener ) {
+    synchronized ( listeners ) {
+      listeners.remove( _listener );
+    }
+  }
+
+  @Override
+  public XAResource getXAResource( ) throws ResourceException {
+    return null;
+  }
+
+  @Override
+  public LocalTransaction getLocalTransaction( ) throws ResourceException {
+    throw new ResourceException( "Local transactions not supported" );
+  }
+
+  @Override
+  public ManagedConnectionMetaData getMetaData( ) throws ResourceException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void setLogWriter( final PrintWriter _out ) throws ResourceException {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public PrintWriter getLogWriter( ) throws ResourceException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
---
+++ org.pi4j.jee.ra/src/test/java/org/pi4j/jee/ra/Pi4JManagedConnectionTest.java
@@ -0,0 +1,109 @@
+package org.pi4j.jee.ra;
+
+/*
+ * #%L
+ * org.pi4j.jee.ra
+ * %%
+ * Copyright (C) 2015 Greg Lanthier
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertThat;
+
+import java.io.PrintWriter;
+
+import javax.resource.ResourceException;
+import javax.resource.cci.Connection;
+import javax.resource.spi.ConnectionEvent;
+import javax.resource.spi.ConnectionEventListener;
+import javax.resource.spi.ConnectionRequestInfo;
+import javax.security.auth.Subject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+@RunWith( MockitoJUnitRunner.class )
+public class Pi4JManagedConnectionTest {
+
+  @Mock private PrintWriter logWriter;
+
+  @Mock private ConnectionRequestInfo requestInfo;
+
+  @Mock private ConnectionEventListener m_listener;
+
+  @InjectMocks private Pi4JManagedConnection managedConnection;
+
+  @Test
+  public void testGetConnection( ) throws Exception {
+    Subject subject = new Subject( );
+    Connection connection = ( Connection ) managedConnection.getConnection( subject, requestInfo );
+    connection.close( );
+  }
+
+  @Test
+  public void testDestroy( ) throws Exception {
+    managedConnection.destroy( );
+  }
+
+  @Test
+  public void testCleanup( ) throws Exception {
+    managedConnection.cleanup( );
+  }
+
+  @Test
+  public void testAssociateConnection( ) throws Exception {
+    managedConnection.associateConnection( null );
+  }
+
+  @Test
+  public void testAddConnectionEventListener( ) throws Exception {
+    managedConnection.addConnectionEventListener( m_listener );
+    managedConnection.addConnectionEventListener( m_listener );
+    managedConnection.removeConnectionEventListener( m_listener );
+    managedConnection.removeConnectionEventListener( m_listener );
+    managedConnection.removeConnectionEventListener( m_listener );
+  }
+
+  @Test
+  public void testGetXAResource( ) throws Exception {
+    assertThat( managedConnection.getXAResource( ), is( nullValue() ) );
+  }
+
+  @Test( expected = ResourceException.class )
+  public void testGetLocalTransaction( ) throws Exception {
+    managedConnection.getLocalTransaction( );
+  }
+
+  @Test
+  public void testGetMetaData( ) throws Exception {
+    assertThat( managedConnection.getMetaData( ), is( nullValue() ) );
+  }
+
+  @Test
+  public void testSetLogWriter( ) throws Exception {
+    managedConnection.setLogWriter( null );
+  }
+
+  @Test
+  public void testGetLogWriter( ) throws Exception {
+    assertThat( managedConnection.getLogWriter( ), is( nullValue() ) );
+  }
+
+}
